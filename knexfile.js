012235f99ab19ac6dcfe72f180e2f68698fecd92
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-06-01 09:46:31 -0700

    Finished initial migration, build basic server

---
+++ knexfile.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/projects.db3',
+    },
+    useNullAsDefault: true
+  }
+};
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const helmet = require('helmet');
+
+// const ProjectsRouter = require('./projects/projectsRouter.js');
+// const ActionsRouter = require('./actions/actionsRouter.js');
+
+const server = express();
+server.use(express.json());
+server.use(helmet());
+
+// server.use('/api/projects', ProjectsRouter);
+// server.use('/api/actions', ActionsRouter);
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Projects & Actions API!</h2>`)
+  });
+
+module.exports = server;
